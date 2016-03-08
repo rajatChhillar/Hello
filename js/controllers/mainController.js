4f311ad1b80392a6061ec9b9e45ffd76f833fc0d
Rajat Chhillar <rchhil@sapient.com>
2016-03-08 16:08:10 +0530

    done with the remove from table

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,38 +1,50 @@
 app.controller('MainController', ['$scope',function($scope) { 
-  $scope.title = 'FIRST ANGULAR APP'; 
-  $scope.promo='GO GO GO';
+  $scope.title = 'ADD AND SHOW USERS'; 
   $scope.checked=false;
   $scope.persons= 
   [{ 
     name: 'Gaurav Gera', 
     OracleID: 117401, 
-    Designation: 'AL1'
+    Designation: 'AL1',
+    status:true,
+    checked:$scope.checked
   },
   { 
     name: 'ArpitJain', 
     OracleID: 117402, 
-    Designation: 'AL2'
+    Designation: 'AL2',
+    status:false,
+    checked:$scope.checked
   },
   { 
     name: 'Ankur Poria', 
     OracleID: 117403, 
-    Designation: 'AL3'
+    Designation: 'AL3',
+    status:true,
+    checked:$scope.checked
   },
   { 
     name: 'Rajat Chhillar', 
     OracleID: 117404, 
-    Designation: 'AL4'
+    Designation: 'AL4',
+    status:true,
+    checked:$scope.checked
   },
   { 
     name: 'Angular JS', 
     OracleID: 117405, 
-    Designation: 'AL5'
+    Designation: 'AL5',
+    status:false,
+    checked:$scope.checked
   }
-]
-$scope.add=function(index) { 
-  
-};
-  $scope.remove=function(index) { 
-  
+];
+$scope.remove=function() { 
+  angular.forEach($scope.persons,function(value){
+   if(value.checked){
+      value.status=false;
+      console.log(value);
+    }
+    
+  });
 };
 }]);
